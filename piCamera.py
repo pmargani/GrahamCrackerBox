7e4e9f900f9c0430c12c25d6d360c7552b205b8a
Paul Marganian <pmargani@nrao.edu>
2021-01-01 15:52:54 -0500

    piCamera.py tries to detect a face

--- piCamera.py
+++ piCamera.py
@@ -2,6 +2,7 @@ import time
 
 from picamera import PiCamera
 
+from objDetect import detectFace
 from gmail import readSpecialEmails, getService, send_message, create_message_with_attachment
 from settings import SUBJ, SENDER, FROM
 
@@ -19,14 +20,15 @@ def captureImage(fn=None):
     # take a picture and save it
     c.capture(fn)
 
-def sendImage(fn):
+def sendImage(fn, faceDetected=False):
 
     service = getService()
     subj = SUBJ + " reply"
-    msg = create_message_with_attachment(FROM, FROM, subj, "None", fn)
+    txt = "Face Detected: " + str(faceDetected)
+    msg = create_message_with_attachment(FROM, FROM, subj, txt, fn)
     send_message(service, FROM, msg)
 
-def emailImage():
+def emailImage(faceDetect=True):
 
     # is it time to take an image?
     msgs = readSpecialEmails(SUBJ, SENDER)
@@ -35,11 +37,19 @@ def emailImage():
         # yes; take picture
         fn = "image.jpg"
         captureImage(fn)
+        
+        # look for a face?
+        faceDetected = False
+        if faceDetect:
+            rects = detectFace(fn)
+            faceDetected = len(rects) > 0
+        
         # and send it
-        sendImage(fn)
+        sendImage(fn, faceDetected)
         print("Sent message with image!")
 
 def main():
+    print("piCamera.py")
     emailImage()
     #captureImage("test.jpg")
 
